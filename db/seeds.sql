93fe829bf58a0c469575162ab7d5aec95fc7a698
mischadab <mischadabrowski@gmail.com>
2021-03-07 14:43:02 -0500

    added seeds to database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
